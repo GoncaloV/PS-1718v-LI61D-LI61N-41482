b9e950c78c3dd1d96516258797e929c87149bf5b
GoncaloV <gonvel92@gmail.com>
2018-08-30 15:27:29 +0100

    Added batch files for building and running the application.

---
+++ README.txt
@@ -0,0 +1,6 @@
+To run the application:
+1. Open the file build.bat.
+2. Open the file run.bat.
+3. Open a browser of your choice and enter localhost:8080 in your address bar.
+4. The application may take a few seconds to start up.
+5. Alternatively, go to https://diario-de-jogos.herokuapp.com/.
--- src/test/java/org/gamelog/PlayerTests.java
+++ src/test/java/org/gamelog/PlayerTests.java
@@ -1,19 +1,10 @@
 package org.gamelog;
 
-import org.gamelog.model.Player;
-import org.gamelog.service.PlayerService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Collections;
-import java.util.concurrent.atomic.AtomicReference;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
